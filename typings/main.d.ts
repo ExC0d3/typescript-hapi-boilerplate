1e12c8e56ff1f57938758955761711937b6217b9
ExC0d3 <sharma.abhinav503@gmail.com>
2016-03-25 12:08:14 +0530

    added node typigns

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="main/ambient/hapi/index.d.ts" />
 /// <reference path="main/ambient/joi/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="main/definitions/lodash/index.d.ts" />
